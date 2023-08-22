1fcbe1441ee8d7d9c59d3eb128d152e428ed77fd
Carvs10 <joaovcl10@hotmail.com>
2023-08-22 16:36:35 -0300

    header

--- module_21/src/main.js
+++ module_21/src/main.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
 
+  const heroSection = document.querySelector(".hero");
+  const heroHeight = heroSection.clientHeight;
+
+  window.addEventListener("scroll", () => {
+    const currentPos = window.scrollY;
+
+    if (currentPos < heroHeight) {
+      hideHeader();
+    } else {
+      showHeader();
+    }
+  });
+
+  //Atrações
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (button) => {
       const target = button.target.dataset.tabButton;
@@ -11,7 +26,28 @@ document.addEventListener("DOMContentLoaded", () => {
       button.target.classList.add("shows__tabs__button--is-active");
     });
   }
+
+  //faq accordion
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", toggleQuestion);
+  }
 });
+
+function hideHeader() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
+}
+
+function showHeader() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
+}
+function toggleQuestion(element) {
+  const classes = "faq__questions__item--is-open";
+  const parentElement = element.target.parentNode;
+
+  parentElement.classList.toggle(classes);
+}
 
 function removeButtonClass() {
   const buttons = document.querySelectorAll("[data-tab-button]");
