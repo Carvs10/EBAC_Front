814e317569c968dbab871823ab1428ef5d7a6056
Carvs10 <joaovcl10@hotmail.com>
2023-09-01 11:21:47 -0300

    Exercicio

---
+++ module_26/exercicio/exercicio.ts
@@ -0,0 +1,7 @@
+function multiplication(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+function saysHello(nameGreeting: string): string {
+  return `Olá ${nameGreeting}`;
+}
--- module_26/src/dom.js
+++ module_26/src/dom.js
@@ -2,4 +2,4 @@
 const inputElement = document.getElementById("nome");
 const formulario = document.getElementsByTagName("form");
 const button = document.getElementById("btn-enviar");
-button === null || button === void 0 ? void 0 : button.addEventListener("click", () => { });
+button === null || button === void 0 ? void 0 : button.addEventListener("click", (e) => { });
