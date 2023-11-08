c16f61f490a15f6d0108ea8138f0f6cbb8eae3ff
Carvs10 <joaovcl10@hotmail.com>
2023-11-08 11:20:45 -0300

    creating media query

--- react/portifolio/src/containers/About/index.tsx
+++ react/portifolio/src/containers/About/index.tsx
@@ -1,5 +1,6 @@
 import Title from '../../components/Title'
 import Paragraph from '../../components/Paragraph'
+import { GithubSection } from './styles'
 
 const About = () => (
   <section>
@@ -9,6 +10,10 @@ const About = () => (
       incidunt in dolorum ipsum suscipit illum error repudiandae iste
       voluptatum! Aliquid ullam quidem aliquam nemo eos autem nobis nisi amet.
     </Paragraph>
+    <GithubSection>
+      <img src="https://github-readme-stats.vercel.app/api?username=Carvs10&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Carvs10&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSection>
   </section>
 )
 
